4186255ff33cefa131ed8b2d14cd6b33e32548ec
Christian <150555815+ChristoTF2@users.noreply.github.com>
2024-06-06 22:48:14 -0500

    Feat(crop):Added unit Test Controller

---
+++ AgroSolutions.Presentation.Test/Crops/AdivsorControllerUnitTest.cs
@@ -0,0 +1,79 @@
+﻿using _1_API.Response;
+using Agrosolutions.Domain.Crops.Model.Queries;
+using AutoMapper;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Presentation.Controllers;
+
+namespace Presetation.Test;
+
+public class AdivsorControllerUnitTest
+{
+    [Fact]
+    public async Task GetAllAdvisersAsync_ResultOk()
+    {
+        // Arrange
+        var mockMapper = new Mock<IMapper>();
+        var mockAdviserQueryService = new Mock<ICropQueryService>();
+        var mockAdviserCommandService = new Mock<ICropCommandService>();
+
+        var fakeAdvisersList = new List<AdviserResponse>()
+        {
+            new AdviserResponse
+            {
+                Id = 1,
+                Nombre = "Ana Torres",
+                Descripcion = "Experta en cultivos orgánicos y sostenibilidad.",
+                Calificacion = 4.5
+            },
+            new AdviserResponse
+            {
+                Id = 2,
+                Nombre = "Carlos Gómez",
+                Descripcion = "Especialista en manejo de suelos y fertilización.",
+                Calificacion = 4.7
+            },
+            new AdviserResponse
+            {
+                Id = 3,
+                Nombre = "Luisa Fernández",
+                Descripcion = "Asesora en tecnologías agrícolas y maquinaria.",
+                Calificacion = 4.8
+            } 
+        };
+
+        mockAdviserQueryService.Setup(t => t.Handle(It.IsAny<GetAllAsesoresQuery>())).ReturnsAsync(fakeAdvisersList);
+
+        var controller = new CropController(mockAdviserQueryService.Object, mockAdviserCommandService.Object, mockMapper.Object);
+
+        // Act
+        var result = await controller.GetAllAdvisersAsync();
+
+        // Assert
+        var okResult = Assert.IsType<OkObjectResult>(result);
+        var returnValue = Assert.IsType<List<AdviserResponse>>(okResult.Value);
+        Assert.NotEmpty(returnValue);
+    }
+
+    [Fact]
+    public async Task GetAllAdvisersAsync_ResultNotFound()
+    {
+        // Arrange
+        var mockMapper = new Mock<IMapper>();
+        var mockAdviserQueryService = new Mock<ICropQueryService>();
+        var mockAdviserCommandService = new Mock<ICropCommandService>();
+
+        var fakeAdvisersList = new List<AdviserResponse>();
+
+        mockAdviserQueryService.Setup(t => t.Handle(It.IsAny<GetAllAsesoresQuery>())).ReturnsAsync(fakeAdvisersList);
+
+        var controller = new CropController(mockAdviserQueryService.Object, mockAdviserCommandService.Object, mockMapper.Object);
+
+        // Act
+        var result = await controller.GetAllAdvisersAsync();
+
+        // Assert
+        Assert.IsType<NotFoundResult>(result);
+    }
+}
---
+++ AgroSolutions.Presentation.Test/Crops/CropControllerUnitTest.cs
@@ -0,0 +1,61 @@
+﻿using _1_API.Response;
+using Agrosolutions.Domain.Crops.Model.Queries;
+using AutoMapper;
+using Domain;
+
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Presentation.Controllers;
+
+namespace Presetation.Test;
+
+public class CropControllerUnitTest
+{
+    [Fact]
+    public async Task GetAllCropsAsync_ResultOk()
+    {
+        // Arrange
+        var mockMapper = new Mock<IMapper>();
+        var mockcropQueryService = new Mock<ICropQueryService>();
+        var mockCropCommandService = new Mock<ICropCommandService>();
+
+        var fakeCropsList = new List<CropsResponse>()
+        {
+            new CropsResponse() 
+        };
+
+        mockcropQueryService.Setup(t => t.Handle(It.IsAny<GetAllCultivosQuery>())).ReturnsAsync(fakeCropsList);
+
+        var controller = new CropController(mockcropQueryService.Object, mockCropCommandService.Object, mockMapper.Object);
+
+        // Act
+        var result = await controller.GetAllCropsAsync();
+
+        // Assert
+        var okResult = Assert.IsType<OkObjectResult>(result);
+        var returnValue = Assert.IsType<List<CropsResponse>>(okResult.Value);
+        Assert.NotEmpty(returnValue);
+    }
+
+    [Fact]
+    public async Task GetAllCropsAsync_ResultNotFound()
+    {
+        // Arrange
+        var mockMapper = new Mock<IMapper>();
+        var mocKCropQueryService = new Mock<ICropQueryService>();
+        var mockCropCommandService = new Mock<ICropCommandService>();
+
+        var fakeCropsList = new List<CropsResponse>();
+
+        mocKCropQueryService.Setup(t => t.Handle(It.IsAny<GetAllCultivosQuery>())).ReturnsAsync(fakeCropsList);
+
+        var controller = new CropController(mocKCropQueryService.Object, mockCropCommandService.Object, mockMapper.Object);
+
+        // Act
+        var result = await controller.GetAllCropsAsync();
+
+        // Assert
+        Assert.IsType<NotFoundResult>(result);
+    }
+
+}
